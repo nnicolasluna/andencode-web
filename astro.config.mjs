1b0fa021d788e8cbf2bcd44b00fe9c7e5f2f0cab
Joel Nicolas Luna <nnicolasluna@gmail.com>
2026-05-22 18:42:16 -0400

    despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://tu-nnicolasluna.github.io",
+  base: "/andencode-web",
 });
